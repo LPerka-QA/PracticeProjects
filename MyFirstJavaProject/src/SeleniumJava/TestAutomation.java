68817762c6aa0fff58920c4a7c32c4afc9bcd4b3
LPerka-QA <lokendernath.mudiraj@gmail.com>
2021-10-05 11:42:17 -0400

    Second Commit and Push

--- MyFirstJavaProject/src/SeleniumJava/TestAutomation.java
+++ MyFirstJavaProject/src/SeleniumJava/TestAutomation.java
@@ -4,6 +4,9 @@ public class TestAutomation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+System.out.println("First Commit and Push Successful");
+
+System.out.println("Verify Second Commit and Push");
 
 	}
 
